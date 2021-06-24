102c2cd33f3b616438c53d64b05be274668d5baa
vagrant <hbhandal80@hotmail.com>
2021-06-24 05:16:43 +0000

    Added admin security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+  
+  include HttpAuthConcern
+
+  http_basic_authenticate_with name: ENV['username'], password: ENV['password']
+
   def show
   end
 end
